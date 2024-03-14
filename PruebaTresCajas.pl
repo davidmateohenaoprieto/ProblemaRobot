71abc61561bc5efe8d76ac4e9de100efe78a2571
davidmateohenaoprieto <davidmhenaop@javeriana.edu.co>
2024-03-14 10:44:37 -0500

    Version Final

--- PruebaTresCajas.pl
+++ PruebaTresCajas.pl
@@ -5,7 +5,6 @@ habitacion(h2).
 
 caja(azul).
 caja(roja).
-caja(verde).
 
 :- dynamic(ubicacion_inicial/1).
 ubicacion_inicial(h1).
@@ -13,10 +12,8 @@ ubicacion_inicial(h1).
 :- dynamic(en_habitacion/2).
 en_habitacion(azul, h1).
 en_habitacion(roja, h1).
-en_habitacion(verde, h1).
 
 :- dynamic(valor/2).
-valor(verde, 2).
 valor(roja, -1).
 valor(azul, 1).
 
@@ -34,16 +31,6 @@ mover(Robot, Desde, Hacia) :-
     asserta(ubicacion_inicial(Hacia)),  % Afirmar que el robot está en la habitación a la que se ha movido
     write('El robot se ha movido de '), write(Desde), write(' a '), write(Hacia), nl.  % Imprimir un mensaje de movimiento
 
-% Regla para que el robot agarre una caja
-agarrar(Robot, Caja) :-
-    ubicacion_inicial(Habitacion),  % Obtener la ubicación actual del robot
-    en_habitacion(Caja, Habitacion),  % Verificar si la caja está en la misma habitación que el robot
-    valor(Caja, Valor),  % Verificar si la caja tiene valor igual a 1
-    Valor > 0,
-    retract(en_habitacion(Caja, Habitacion)),  % Retirar el hecho de que la caja está en la habitación actual
-    write('El robot ha agarrado la caja '), write(Caja), nl.  % Imprimir un mensaje de agarrar
-
-
 % Regla para que el robot suelte una caja
 soltar(Robot, Caja) :-
     ubicacion_inicial(Habitacion),  % Obtener la ubicación actual del robot
@@ -52,28 +39,11 @@ soltar(Robot, Caja) :-
     asserta(valor(Caja,0)),  % Afirmar que el robot está en la habitación a la que se ha movido
     write('El robot ha soltado la caja '), write(Caja), nl.  % Imprimir un mensaje de soltar
 
-% Regla para listar todas las cajas con valor positivo
-listar :-
-    ubicacion_inicial(Habitacion),  % Obtener la ubicación actual del robot
-    listar_cajas_en_habitacion(Habitacion).  % Llamar a la regla auxiliar
-
-% Regla auxiliar para listar cajas en una habitación
-listar_cajas_en_habitacion(Habitacion) :-
-    en_habitacion(Caja, Habitacion),  % Obtener todas las cajas en la habitación actual
-    valor(Caja, Valor),  % Obtener el valor de la caja
-    Valor > 0,  % Verificar si el valor es positivo
-    write('Caja con valor positivo: '), write(Caja), nl,  % Imprimir el nombre de la caja
-    fail. % Falla para forzar el backtracking y buscar mas cajas positivas
-
-listar_cajas_en_habitacion(_). % Regla base para terminar la recursión
-
 listar_ordenado_para_resolver(CajasOrdenadas) :-
     ubicacion_inicial(Habitacion),  % Obtener la ubicación actual del robot
     findall(Caja-Valor, (en_habitacion(Caja, Habitacion), valor(Caja, Valor), Valor > 0), CajasPositivas), % Recolectar cajas positivas
     sort(2, @>=, CajasPositivas, CajasOrdenadas), % Ordenar las cajas positivas de mayor a menor según su valor
     imprimir_cajas_ordenadas(CajasOrdenadas). % Imprimir las cajas ordenadas
-
-
 
 % Regla para imprimir las cajas ordenadas
 imprimir_cajas_ordenadas([]). % Caso base: lista vacía, no hay cajas que imprimir
@@ -87,7 +57,7 @@ mover_caja_a_habitacion2(Caja) :-
     HabitacionRobot = HabitacionCaja,  % Verificar si el robot y la caja están en la misma habitación
     retract(en_habitacion(Caja, HabitacionCaja)),  % Retirar la caja de la habitación actual
     asserta(en_habitacion(Caja, h2)),  % Colocar la caja en la habitación 2
-    write('El robot ha movido la caja '), write(Caja), write(' a la Habitación 2.'), nl,
+    write('El robot ha agarrrado la caja '), write(Caja), nl,
     mover(Robot, HabitacionRobot, h2),  % Mover al robot a la habitación 2
     soltar(Robot, Caja), % Soltar la caja en la habitación 2
     !. % Cortar para evitar backtracking
@@ -116,6 +86,12 @@ eliminar_caja(Caja, [OtraCaja-_|Resto], [OtraCaja-_|RestoSinCaja]) :- % Mantener
     eliminar_caja(Caja, Resto, RestoSinCaja).
 
 resolver :-
+    % ENTREGA 1 PROYECTO IA (BRAYAN FAJARDO-GABRIEL CORONADO-MATEO HENAO)
+    write('ENTREGA 1 PROYECTO IA (BRAYAN FAJARDO-GABRIEL CORONADO-MATEO HENAO)'), nl, nl,
+    write('LISTADO DE CAJAS A RECOGER---------------------------------------------'), nl,
     listar_ordenado_para_resolver(CajasOrdenadas),  % Generar lista de cajas ordenadas por valor
+    write('-----------------------------------------------------------------------'), nl,nl,
+    write('LISTADO DE MOVIMIENTOS DEL ROBOT---------------------------------------'), nl,
     mover_cajas_ordenadas(CajasOrdenadas), % Mover las cajas ordenadas a la habitación 2
+    write('-----------------------------------------------------------------------'), nl, nl,  
     !. % Cortar para evitar backtracking y finalizar el proceso
